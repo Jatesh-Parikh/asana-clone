527473066c2772f94842dcbed3bd0f087e06975b
Jatesh Parikh <jateshparikh97@gmail.com>
2024-01-31 22:27:23 +0530

    First Section Done.

---
+++ app/(landing-page)/page.tsx
@@ -0,0 +1,13 @@
+import FirstSection from "./FirstSection/page";
+import Navbar from "./Navbar/navbar";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <FirstSection />
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ app/contact/page.tsx
@@ -0,0 +1,5 @@
+const ContactPage = () => {
+  return <div>Hi. This is a contact page</div>;
+};
+
+export default ContactPage;
